5ac0eb4d032660348f0a696bb4f91a65189b6222
Nikolay Kugut <nikolay.kugut@gmail.com>
2024-05-13 02:04:12 +0200

    add comment and readme

--- components/logger/config/logger.php
+++ components/logger/config/logger.php
@@ -1,13 +1,13 @@
 <?php
 return [
-	'class' => app\components\logger\Logger::class,
-	'defaultDriver' => 'file',
-	'loggerDrivers' => [
-		'file' => app\components\logger\drivers\FileDriver::class,
-		'db' => app\components\logger\drivers\DataBaseDriver::class,
-		'email' => [
-			'class' => app\components\logger\drivers\EmailDriver::class,
-			'email' => 'test@test.com'
+	'class' => app\components\logger\Logger::class, // Specifies the class for the logger component
+	'defaultDriver' => 'file', // Sets the default driver for logging
+	'loggerDrivers' => [ // Defines different logger drivers
+		'file' => app\components\logger\drivers\FileDriver::class, // File logging driver
+		'db' => app\components\logger\drivers\DataBaseDriver::class, // Database logging driver
+		'email' => [ // Email logging driver configuration
+			'class' => app\components\logger\drivers\EmailDriver::class, // Email logging driver class
+			'email' => 'test@test.com' // Email address for logging
 		]
 	]
 ];
